87e3e8945d3d77775a4c66122de184da97bcf849
hughstimson <hugh@hughstimson.org>
2016-02-16 16:24:26 -0800

    Fixes issue with Terms and Conditions interstitial.
    Hobart council added a "Terms and Conditions" click-through page that
    was blocking the scraper. This update
    
    - Adds the mechanize library as a requirement.
    - Uses mechanize to click through the Terms form.
    - Takes advantage of changes in the available data to always grab all
    applications.
    - Fixes scraping of the council contact field.
    - Removes newlines in the council contact field.
    - Removes some unused dependencies.

--- hobart.rb
+++ hobart.rb
@@ -21,7 +21,7 @@ agent.submit(form, button)
 # Now we can get to the page with the data.
 page = agent.get('https://apply.hobartcity.com.au/CurrentlyAdvertised')
 
-applications = page.search('.result').collect do |result|
+applications = doc.css('div.result').collect do |result|
   page_info = {}
   # page_info['address'] = result.css('a')[0].text.strip  # Addresses are stored inconsistently here. Use detail page.
   page_info['council_reference'] = result.css('a')[1].text.strip
@@ -40,26 +40,52 @@ applications = page.search('.result').collect do |result|
 
   # Now scrape the detail page for additional information and a more consistently formatted address.
   # (Adding this step slows the scraper significantly.)
-  detail_page = agent.get(page_info['info_url'])
+  page = Nokogiri::HTML(open(page_info['info_url'], :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE))
 
-  contact_div = detail_page.at_css('div#b_ctl00_ctMain_info_Officer')
+  contact_div = page.at_css('div#b_ctl00_ctMain1_info_Officer')
   unless contact_div.nil?
-    page_info['council_contact'] = contact_div.text.gsub(/\s+/, ' ').strip  # Remove carriage returns.
+    page_info['council_contact'] = contact_div.text.strip
   end
 
-  location_div = detail_page.at_css('div#b_ctl00_ctMain_info_prop')
+  location_div = page.at_css('div#b_ctl00_ctMain1_info_prop')
   unless location_div.nil?  # The location will always be provided though right?
     page_info['address'] = location_div.at_css('a').text
   end
 
+  pdfs = Array.new
+  table = page.at_css('table')
+  unless table.nil?  # Sometimes there is no documents table.
+    table.css('tr').collect do |tr|
+      pdf_url = tr.css('td')[0].at_css('a')['href']
+      pdf_url.slice!('../..')
+      pdf_url = 'https://apply.hobartcity.com.au' + pdf_url
+      pdf_name = tr.css('td')[1].text.strip
+      pdf_date = tr.css('td')[2].text.strip
+      pdf = {
+          url: pdf_url,
+          name: pdf_name,
+          date: pdf_date
+      }
+      pdfs << pdf
+    end
+  end
+  page_info['other_info'] = {
+      documents: pdfs
+  }
+
   page_info
 end
 
-applications.each do |record|
-  if (ScraperWikiMorph.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
-    ScraperWiki.save_sqlite(['council_reference'], record)
-    puts 'Saving record ' + record['council_reference']
-  else
-    puts 'Skipping already saved record ' + record['council_reference']
-  end
-end
+# Leave this uncommented to output to JSON.
+json = applications.to_json
+puts json
+
+# # Leave this uncommented to output to sqlite (i.e. for planningalerts.org.au)
+# applications.each do |record|
+#   if (ScraperWikiMorph.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
+#     ScraperWiki.save_sqlite(['council_reference'], record)
+#     puts 'Saving record ' + record['council_reference']
+#   else
+#     puts 'Skipping already saved record ' + record['council_reference']
+#   end
+# end
