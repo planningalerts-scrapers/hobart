35aab570a16eea70ca944bd9ba9f527ca55a298e
Matthew Landauer <mlandauer@gmail.com>
2019-06-10 01:32:21 +1000

    Upgrade to latest version of the gem

--- scraper.rb
+++ scraper.rb
@@ -1,9 +1,3 @@
 require "icon_scraper"
 
-IconScraper.scrape_with_params(
-  url: "https://apply.hobartcity.com.au",
-  period: "last14days",
-  types: ["PLN"]
-) do |record|
-  IconScraper.save(record)
-end
+IconScraper.scrape_and_save(:hobart)
